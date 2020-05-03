8d1120d25cbc6aebbbd0ed412d30674c88cd586a
kumarmo2 <mohitkumar1015795@gmail.com>
2020-05-04 00:47:14 +0530

    started receiving message events.

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,68 @@
-use futures::future::join_all;
-use lapin::{options::*, types::FieldTable};
-use manager::RabbitMqManager;
-use smol::{self, Task};
+use chat_common_types::events::MessageEvent;
+use lapin::{message::Delivery, options::*, types::FieldTable, Consumer};
+use manager::{ChannelManager, PooledConnection, RabbitMqManager};
+use num_cpus;
+use serde_json::from_str;
+use smol::{self, block_on, Task};
 
 fn main() {
     println!("hello world");
+    // TODO: retrieve from the config based on environment.
     let addr = "amqp://guest:guest@127.0.0.1:5672/%2f";
     let rabbit = RabbitMqManager::new(addr);
-    let rabbit_sub_cloned = rabbit.clone();
+    // let rabbit_sub_cloned = rabbit.clone();
+    let cpus = num_cpus::get().max(1);
+    for _ in 0..cpus {
+        std::thread::spawn(move || smol::run(futures::future::pending::<()>()));
+    }
 
-    smol::run(async move {
-        let consumer_task = start_consumer(rabbit_sub_cloned, "hello");
-        join_all(vec![consumer_task]).await;
-    });
+    block_on(start_consumer(rabbit, "messages"));
 }
 
-fn start_consumer(rabbit: RabbitMqManager, queue_name: &'static str) -> Task<()> {
-    return Task::spawn(async move {
-        loop {
-            let channel = rabbit.get_channel_pool().get().unwrap();
-            let consumer = channel
-                .basic_consume(
-                    queue_name,
-                    "message_service",
-                    BasicConsumeOptions::default(),
-                    FieldTable::default(),
-                )
-                .await
-                .unwrap();
-            for delivery in consumer {
-                println!("message received",);
-                if let Ok(delivery) = delivery {
-                    channel
-                        .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
-                        .await
-                        .expect("error while acknowledging the message")
+async fn start_consumer(rabbit: RabbitMqManager, queue_name: &'static str) {
+    loop {
+        let channel: PooledConnection<ChannelManager> = rabbit.get_channel_pool().get().unwrap();
+        println!("got channel");
+        // let channel = rabbit.get_channel_pool().get().unwrap();
+        let consumer: Consumer = channel
+            .basic_consume(
+                queue_name,
+                "message_service",
+                BasicConsumeOptions::default(),
+                FieldTable::default(),
+            )
+            .await
+            .unwrap();
+        println!("message received");
+        Task::spawn(process_messages(consumer, channel)).detach();
+    }
+}
+
+async fn process_messages(consumer: Consumer, channel: PooledConnection<ChannelManager>) {
+    for delivery in consumer {
+        println!(
+            "message received, thread_id: {:?}",
+            std::thread::current().id()
+        );
+        let d: Delivery;
+        if let Ok(delivery) = delivery {
+            d = delivery;
+            let string;
+            match String::from_utf8(d.data) {
+                Ok(result) => {
+                    string = result;
+                }
+                Err(reason) => {
+                    println!("could not deserialize message: {}", reason);
+                    break;
                 }
             }
+            let message_event = from_str::<MessageEvent>(&string);
+            println!("message event: {:?}", message_event);
+            channel
+                .basic_ack(d.delivery_tag, BasicAckOptions::default())
+                .await
+                .expect("error while acknowledging the message")
         }
-    });
+    }
 }
-
-// fn start_publisher(rabbit: RabbitMqManager) -> Task<()> {
-//     // let rabbit_pub_cloned = rabbit.clone();
-//     return Task::spawn(async move {
-//         // let publish_timeout = Duration::from_millis(2000);
-//         let mut counter = 0;
-//         loop {
-//             let result = rabbit
-//                 .publish_message_to_queue_async(
-//                     "hello",
-//                     &DummyPayload {
-//                         input: "kumarmo2".to_string(),
-//                     },
-//                 )
-//                 .await;
-//             match result {
-//                 Ok(is_published) => match is_published {
-//                     true => {
-//                         println!("message sent: {}", counter);
-//                         counter = counter + 1;
-//                     }
-//                     false => println!("error confirming message publish"),
-//                 },
-//                 Err(reason) => {
-//                     println!("error occured: {:?}", reason);
-//                 }
-//             }
-//             // Timer::after(publish_timeout).await;
-//         }
-//     });
-// }
-
-// #[derive(Serialize)]
-// struct DummyPayload {
-//     input: String,
-// }
