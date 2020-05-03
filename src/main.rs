bd3f98873ce4397c860643992eb15026ca85497a
kumarmo2 <mohitkumar1015795@gmail.com>
2020-05-04 03:09:00 +0530

    made performance improvements.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use chat_common_types::events::MessageEvent;
-use lapin::{message::Delivery, options::*, types::FieldTable, Consumer};
+use lapin::{message::Delivery, options::*, types::FieldTable, Channel, Consumer};
 use manager::{ChannelManager, PooledConnection, RabbitMqManager};
 use num_cpus;
 use serde_json::from_str;
 use smol::{self, block_on, Task};
+use std::time::Instant;
 
 fn main() {
     println!("hello world");
     // TODO: retrieve from the config based on environment.
     let addr = "amqp://guest:guest@127.0.0.1:5672/%2f";
+    // TODO: update the RabbitMqManager to accept the max number of connections and channels.
     let rabbit = RabbitMqManager::new(addr);
-    // let rabbit_sub_cloned = rabbit.clone();
     let cpus = num_cpus::get().max(1);
     for _ in 0..cpus {
         std::thread::spawn(move || smol::run(futures::future::pending::<()>()));
@@ -20,10 +21,19 @@ fn main() {
 }
 
 async fn start_consumer(rabbit: RabbitMqManager, queue_name: &'static str) {
+    let mut channel: PooledConnection<ChannelManager>;
+    println!("will get");
     loop {
-        let channel: PooledConnection<ChannelManager> = rabbit.get_channel_pool().get().unwrap();
+        match rabbit.get_channel_pool().get() {
+            Ok(ch) => {
+                channel = ch;
+            }
+            Err(reason) => {
+                println!("could not get channel, reason: {:?}", reason);
+                return;
+            }
+        }
         println!("got channel");
-        // let channel = rabbit.get_channel_pool().get().unwrap();
         let consumer: Consumer = channel
             .basic_consume(
                 queue_name,
@@ -33,12 +43,13 @@ async fn start_consumer(rabbit: RabbitMqManager, queue_name: &'static str) {
             )
             .await
             .unwrap();
-        println!("message received");
-        Task::spawn(process_messages(consumer, channel)).detach();
+        process_messages(consumer, channel).await;
     }
 }
 
 async fn process_messages(consumer: Consumer, channel: PooledConnection<ChannelManager>) {
+    println!("start of consumer processing");
+    let now = Instant::now();
     for delivery in consumer {
         println!(
             "message received, thread_id: {:?}",
@@ -46,23 +57,30 @@ async fn process_messages(consumer: Consumer, channel: PooledConnection<ChannelM
         );
         let d: Delivery;
         if let Ok(delivery) = delivery {
-            d = delivery;
-            let string;
-            match String::from_utf8(d.data) {
-                Ok(result) => {
-                    string = result;
-                }
-                Err(reason) => {
-                    println!("could not deserialize message: {}", reason);
-                    break;
-                }
-            }
-            let message_event = from_str::<MessageEvent>(&string);
-            println!("message event: {:?}", message_event);
-            channel
-                .basic_ack(d.delivery_tag, BasicAckOptions::default())
-                .await
-                .expect("error while acknowledging the message")
+            Task::spawn(handle_messasge_event(delivery, channel.clone())).detach()
         }
     }
+    println!(
+        "end of consumer processing, total time: {} mins",
+        (now.elapsed().as_secs_f64() / 60.0)
+    );
+}
+
+async fn handle_messasge_event(delivery: Delivery, channel: Channel) {
+    let string;
+    match String::from_utf8(delivery.data) {
+        Ok(result) => {
+            string = result;
+        }
+        Err(reason) => {
+            println!("could not deserialize message: {}", reason);
+            return;
+        }
+    }
+    let message_event = from_str::<MessageEvent>(&string);
+    println!("message event: {:?}", message_event);
+    channel
+        .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
+        .await
+        .expect("error while acknowledging the message")
 }
